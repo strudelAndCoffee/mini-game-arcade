1149aec86008b5b8b8f1b63a08123783ef835881
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-12-15 01:12:19 -0600

    add guessed letter handlers

--- hangman/script.js
+++ hangman/script.js
@@ -1,27 +1,47 @@
 import { words } from './lib/words.js';
 
-const wordToGuess = words[Math.floor(Math.random() * words.length)];
+const wordToGuess = words[Math.floor(Math.random() * words.length)].split('');
 
+const drawingEl = document.querySelector('[data-drawing]');
+const wordEl = document.querySelector('[data-word]');
 const keyboardEl = document.querySelector('[data-keyboard]');
 
-function startInteraction() {
-  document.addEventListener('click', handleMouseClick);
-  document.addEventListener('keydown', (e) => {
-    handleKeyPress(e.key);
-  });
-}
+let wrongGuesses = 0;
 
-function handleMouseClick(e) {
-  if (e.target.matches('[data-key]')) {
-    handleKeyPress(e.target.dataset.key);
-    return;
+function createWordToGuess() {
+  for (let i = 0; i < wordToGuess.length; i++) {
+    let letterEl = document.createElement('span');
+    letterEl.classList.add('letter');
+
+    wordEl.append(letterEl);
   }
-  return;
 }
 
-function handleKeyPress(key) {
+function handleGuessLetter(key) {
   if (!key.match(/^[a-zA-Z]$/)) return;
+
+  wordToGuess.includes(key) ? handleCorrectLetter(key) : handleWrongLetter(key);
+}
+
+function handleCorrectLetter(key) {
   console.log(key);
 }
 
-startInteraction();
+function handleWrongLetter(key) {
+  const bodyPiece = drawingEl.querySelector(
+    `[data-piece="${wrongGuesses + 1}"]`
+  );
+  bodyPiece.classList.remove('hidden');
+  wrongGuesses++;
+}
+
+createWordToGuess();
+console.log(wordToGuess);
+document.addEventListener('click', (e) => {
+  if (e.target.matches('[data-key]')) {
+    handleGuessLetter(e.target.dataset.key);
+  }
+});
+document.addEventListener('keydown', (e) => {
+  handleGuessLetter(e.key);
+});
