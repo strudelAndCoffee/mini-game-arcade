309ca243e2945122980a7736250e54081d6938b9
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-12-17 01:20:31 -0600

    add card html getter

--- card-games/script.js
+++ card-games/script.js
@@ -1,5 +1,9 @@
 import Deck from './deck.js';
 
+const computerCardSlot = document.querySelector('.computer-card-slot');
+
 const deck = new Deck();
 deck.shuffle();
 console.log(deck);
+
+computerCardSlot.append(deck.cards[0].getHTML());
