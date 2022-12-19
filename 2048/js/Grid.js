9f427b2aaa2798debf4e64a1d047bda2ba231974
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-12-18 23:43:24 -0600

    finish movement handling

--- 2048/js/Grid.js
+++ 2048/js/Grid.js
@@ -18,6 +18,10 @@ export default class Grid {
     });
   }
 
+  get cells() {
+    return this.#cells;
+  }
+
   get cellsByColumn() {
     return this.#cells.reduce((cellGrid, cell) => {
       cellGrid[cell.x] = cellGrid[cell.x] || [];
@@ -92,6 +96,13 @@ class Cell {
       this.tile == null ||
       (this.mergeTile == null && this.tile.value === tile.value)
     );
+  }
+
+  mergeTiles() {
+    if (this.tile == null || this.mergeTile == null) return;
+    this.tile.value = this.tile.value + this.mergeTile.value;
+    this.mergeTile.remove();
+    this.mergeTile = null;
   }
 }
 
--- 2048/js/Tile.js
+++ 2048/js/Tile.js
@@ -11,6 +11,10 @@ export default class Tile {
     this.value = value;
   }
 
+  get value() {
+    return this.#value;
+  }
+
   set value(v) {
     this.#value = v;
     this.#tileElement.textContent = v;
@@ -31,5 +35,17 @@ export default class Tile {
   set y(val) {
     this.#y = val;
     this.#tileElement.style.setProperty('--y', val);
+  }
+
+  remove() {
+    this.#tileElement.remove();
+  }
+
+  waitForTransition() {
+    return new Promise((resolve) => {
+      this.#tileElement.addEventListener('transitionend', resolve, {
+        once: true,
+      });
+    });
   }
 }
