456630533c65e3f9bb7c970a8750a8c4a7888d79
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2023-02-17 04:05:02 -0600

    add ship movement

--- games/space-raiders/script.js
+++ games/space-raiders/script.js
@@ -9,14 +9,15 @@ let context
 // ship
 const SHIP_WIDTH = TILE_SIZE * 2
 const SHIP_HEIGHT = TILE_SIZE
-let ship_x = (TILE_SIZE * COLS) / 2 - TILE_SIZE
-let ship_y = TILE_SIZE * ROWS - TILE_SIZE * 2
-let ship = {
-  x: ship_x,
-  y: ship_y,
+const SHIP_START_X = (TILE_SIZE * COLS) / 2 - TILE_SIZE
+const SHIP_START_Y = TILE_SIZE * ROWS - TILE_SIZE * 2
+const ship = {
+  x: SHIP_START_X,
+  y: SHIP_START_Y,
   height: SHIP_HEIGHT,
   width: SHIP_WIDTH,
 }
+let ship_velocity_x = TILE_SIZE
 let ship_img
 
 window.onload = function () {
@@ -32,5 +33,27 @@ window.onload = function () {
   ship_img.src = './assets/images/ship.png'
   ship_img.onload = function () {
     context.drawImage(ship_img, ship.x, ship.y, ship.width, ship.height)
+  }
+
+  requestAnimationFrame(update)
+  document.addEventListener('keydown', moveShip)
+}
+
+function update() {
+  requestAnimationFrame(update)
+  context.clearRect(0, 0, board.width, board.height)
+
+  context.drawImage(ship_img, ship.x, ship.y, ship.width, ship.height)
+}
+
+function moveShip(e) {
+  if (e.code === 'ArrowLeft' && ship.x - ship_velocity_x >= 0) {
+    ship.x -= ship_velocity_x
+  }
+  if (
+    e.code === 'ArrowRight' &&
+    ship.x + ship_velocity_x + ship.width <= board.width
+  ) {
+    ship.x += ship_velocity_x
   }
 }
